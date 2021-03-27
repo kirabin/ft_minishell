e4fba478ca079534ab1dda50fc2fb7e4f0c15c0b
Mitchel Samual <msamual@un-i2.kzn.21-school.ru>
2021-03-27 15:08:53 +0300

    history_list

--- history.c
+++ history.c
@@ -6,7 +6,7 @@
 /*   By: msamual <msamual@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 19:08:08 by msamual           #+#    #+#             */
-/*   Updated: 2021/03/26 17:20:39 by msamual          ###   ########.fr       */
+/*   Updated: 2021/03/27 15:06:49 by msamual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,54 @@ void	push_to_command_history(t_vars *vars, char *command)
 		current->next = node;
 	node->prev = current;
 	node->next = NULL;
-	ft_strlcpy(node->com, command, ft_strlen(command));
+	ft_strlcpy(node->com, command, ft_strlen(command) + 1);
 	vars->history = node;
+}
+
+void	remove_current_input(t_history *history)
+{
+	t_history *tmp;
+
+	tmp = history;
+	while (tmp->next->next)
+		tmp = tmp->next;
+	free(tmp->next);
+	tmp->next = NULL;
+	
+}
+
+t_history	*to_end_of_list(t_history *hist)
+{
+	if (!hist)
+		return (NULL);
+	while (hist->next)
+		hist = hist->next;
+	return (hist);
+}
+
+void	remove_elem_hist(t_history **history)
+{
+	t_history	*prev;
+	t_history	*next;
+	t_history	*tmp;
+
+	tmp = *history;
+	prev = (*history)->prev;
+	next = (*history)->next;
+	if (!prev && !next)
+		*history = NULL;
+	if (prev)
+	{
+		prev->next = next;
+		*history = prev;
+	}
+	if (next)
+	{
+		next->prev = prev;
+		*history = next;
+	}
+	free(tmp);
+	*history = to_end_of_list(*history);
 }
 
 void	print_history(t_history *cur)
@@ -40,9 +86,11 @@ void	print_history(t_history *cur)
 	node = cur;
 	while (node && node->prev)
 		node = node->prev;
+	write(1, "\n", 1);
 	while (node)
 	{
 		ft_putendl(node->com);
 		node = node->next;
 	}
+	write(1, "\n", 1);
 }
--- read_input.c
+++ read_input.c
@@ -6,23 +6,20 @@
 /*   By: msamual <msamual@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 11:21:11 by msamual           #+#    #+#             */
-/*   Updated: 2021/03/26 18:29:39 by msamual          ###   ########.fr       */
+/*   Updated: 2021/03/27 15:05:04 by msamual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	history_prev(t_vars *vars, char *command, char *thread)
+void	history_prev(t_vars *vars)
 {
-	if (vars->history)
+	if (vars->history->prev)
 	{
-		ft_strlcpy(thread, command, ft_strlen(command));
 		tputs(restore_cursor, 1, ft_putint);
 		tputs(tigetstr("ed"), 1, ft_putint);
-		ft_bzero(command, 1000);
-		ft_memmove(command, vars->history->com, ft_strlen(vars->history->com));
 		vars->history = vars->history->prev;
-		ft_putstr(command);
+		ft_putstr(vars->history->com);
 	}
 }
 
@@ -61,11 +58,10 @@ void	backspace(t_vars *vars, char *command)
 	}
 }
 
-void	add_to_command(char *str, char *command, int ret, t_vars *vars)
+void	add_to_command(char *str, int ret, char *command)
 {
 	int		i;
 
-	(void)vars;
 	i = -1;
 	while (*command)
 		command++;
@@ -74,34 +70,43 @@ void	add_to_command(char *str, char *command, int ret, t_vars *vars)
 	write(1, str, ret);
 }
 
-void	execute_command(t_vars * vars, char *command)
+void	execute_command(t_vars * vars)
 {
 	write(1, "\n", 1);
-	if (ft_strlen(command) > 0)
+	if (ft_strlen(vars->history->com) > 0)
 	{
-		push_to_command_history(vars, command);
-		ft_putstr(command);
+		ft_putstr(vars->history->com);
 		write(1, "\n", 1);
+		if (vars->history && vars->history->next)
+		{
+			remove_current_input(vars->history);
+			push_to_command_history(vars, vars->history->com);
+		}
 	}
+	else if (vars->history->next)
+	{
+		remove_elem_hist(&(vars->history));
+		remove_elem_hist(&(vars->history));
+	}
+	else
+		remove_elem_hist(&(vars->history));
+	print_history(vars->history);
 }
 
 void	read_input(t_vars *vars)
 {
 	char	str[100];
-	char	command[1000];
-	char	thread[1000];
 	int		ret;
 
-	ft_bzero(command, 1000);
-	ft_bzero(thread, 1000);
 	tgetent(0, vars->term_name);
 	tputs(save_cursor, 1, ft_putint);
+	push_to_command_history(vars, "");
 	while (1)
 	{
 		ret = read(0, str, 500);
 		str[ret] = 0;
 		if (!ft_strcmp(str, "\e[A"))
-			history_prev(vars, command, thread);
+			history_prev(vars);
 		else if (!ft_strcmp(str, "\e[B"))
 			history_next(vars);
 		else if (!ft_strcmp(str, "\e[D"))
@@ -109,11 +114,11 @@ void	read_input(t_vars *vars)
 		else if (!ft_strcmp(str, "\e[C"))
 			move_right(vars);
 		else if (!ft_strcmp(str, "\177"))
-			backspace(vars, command);
+			backspace(vars, vars->history->com);
 		else if (ft_strcmp(str, "\n"))
-			add_to_command(str, command, ret, vars);
+			add_to_command(str, ret, vars->history->com);
 		else
 			break ;
 	}
-	execute_command(vars, command);
+	execute_command(vars);
 }
