1da84015481c541b2e0f0a4ae2eac9e3b4e42d3c
kirabin <kirabin.one@gmail.com>
2021-04-20 18:51:42 +0300

    redirect_out and pipe_right works together

--- source/exec_redirections.c
+++ source/exec_redirections.c
@@ -18,7 +18,12 @@ void	close_redirections(t_vars *vars, t_command *command)
 {
 	if (command->fd_in != -1 || command->fd_out != -1)
 	{
-		dup2(vars->stdout_copy, STD_OUT);
-		dup2(vars->stdin_copy, STD_IN);
+		if (command->fd_out != -1 && command->pipe_right)
+			;
+		else
+		{
+			dup2(vars->stdout_copy, STD_OUT);
+			dup2(vars->stdin_copy, STD_IN);
+		}
 	}
 }
